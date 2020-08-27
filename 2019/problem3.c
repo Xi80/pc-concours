daaaa04405edb9693ccaf244ce8699c2167eaf1e
Xi80 <h31e26@ed.cc.suzuka-ct.ac.jp>
2020-08-27 13:43:29 +0900

    problem3まで

--- 2019/problem3.c
+++ 2019/problem3.c
@@ -2,3 +2,23 @@
 // Created by c0ntr on 2020/08/27.
 //
 
+#include<stdio.h>
+
+/*
+ * 方針
+ * 最上位の'1'ビットを探す
+ * それ以外のビットを'0'にする
+ */
+
+int main(void) {
+    int input,bit = 0;
+    scanf("%d",&input);
+
+    while(input >> 1){
+            input >>= 1;
+            bit++;
+    }
+
+    printf("%d\n",1 << bit);
+    return 0;
+}
